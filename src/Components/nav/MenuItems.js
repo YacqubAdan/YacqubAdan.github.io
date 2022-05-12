e3589f27469552f791675350cfda4404574ab2fc
Yacquub Adan <yacquubadan.1@gmail.com>
2022-05-12 14:42:06 +0100

    Add menu items file that stores details of the nav links

---
+++ src/Components/nav/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "about",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "experience",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "portfolio",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "contact",
+    url: "#",
+    cName: "nav-links",
+  },
+];
